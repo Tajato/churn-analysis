ea85cda125043ff06dc2ace95937b240117e7c18
Tahj Gordon <tahjalex1@gmail.com>
2025-01-08 10:02:09 -0500

    adding comments to my code
    commenting my code

--- What is the overall churn rate.sql
+++ What is the overall churn rate.sql
@@ -1,12 +1,12 @@
 -- What is the overall churn rate for the company?
 WITH 
     num_churn AS (
-        SELECT COUNT(*) as cnt FROM customers WHERE churn = 'Yes'
+        SELECT COUNT(*) as cnt FROM customers WHERE churn = 'Yes' --Calculate the number of customers who churn. COUNT(*) counts all the rows in the table where churn = 'yes
     ), 
     total_cus AS (
-        SELECT COUNT(*) as cnt FROM customers
+        SELECT COUNT(*) as cnt FROM customers -- calculate the total number of customers. COUNT(*) counts all the rows in the table.
     )
-SELECT (CAST(num_churn.cnt AS FLOAT) / CAST(total_cus.cnt AS FLOAT) * 100) as churn_rate
+SELECT (CAST(num_churn.cnt AS FLOAT) / CAST(total_cus.cnt AS FLOAT) * 100) as churn_rate -- churn rate = num_churn / total_cus.
 FROM num_churn, total_cus;
 
 -- Before this polished query, the churn rate was 0. 
