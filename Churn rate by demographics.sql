2b000a05f2e947019ce0562b8bbae4a49cad9994
Tahj Gordon <tahjalex1@gmail.com>
2025-01-08 10:19:41 -0500

    added comments to the code

--- Churn rate by demographics.sql
+++ Churn rate by demographics.sql
@@ -1,10 +1,11 @@
 --Churn rate by demographics
 --What's the churn rate by gender?
 
-SELECT gender, COUNT(*) AS total_customers, COUNT(CASE WHEN churn = 'Yes' THEN 1 ELSE NULL END) AS churned_customers, 
-       COUNT(CASE WHEN churn = 'Yes' THEN 1 ELSE NULL END) / CAST(COUNT(*) AS FLOAT) * 100 AS churn_rate
+SELECT gender, COUNT(*) AS total_customers, COUNT(CASE WHEN churn = 'Yes' THEN 1 ELSE NULL END) AS churned_customers, -- Case statement allows me to count only rows where the customer has chruned.
+                                                                                                                       --So if churn = 'yes', then 1, which simply means if churn = yes, count this row and ignore the others.
+       COUNT(CASE WHEN churn = 'Yes' THEN 1 ELSE NULL END) / CAST(COUNT(*) AS FLOAT) * 100 AS churn_rate --divide churn customers by total customer in order to get churn rate by gender ( I grouped the results by gender)
 FROM customers
-GROUP BY gender;
+GROUP BY gender; 
 
 -- What's the churn rate by senior citizenship status
 SELECT seniorcitizen, COUNT(*) AS total_customers, COUNT(CASE WHEN churn = 'Yes' THEN 1 ELSE NULL END) AS churned_customers, 
